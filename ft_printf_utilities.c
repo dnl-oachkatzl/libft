de66ad914174b6136a821c9b0c61a6aa286049b2
dnlspr <50107223+dnl-oachkatzl@users.noreply.github.com>
2024-08-22 18:53:15 +0200

    Update ft_printf_utilities.c - deleted duplicate ft_strlen

--- ft_printf_utilities.c
+++ ft_printf_utilities.c
@@ -13,16 +13,6 @@
 #include <stddef.h>
 #include <unistd.h>
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
 int	power_of_ten(int pow)
 {
 	int	result;
